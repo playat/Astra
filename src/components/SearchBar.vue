6c882a57405e1dded9e715029f1f5c82bce7b753
chenqian <playat@163.com>
2025-06-20 17:43:55 +0800

    fix: 搜索框样式

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,18 +1,24 @@
 <template>
   <div
-    class="bg-white-0.5 rounded-full py-2 px-4 w-1/8 hover:w-1/5 transition-all absolute z-10 left-1/2 top-1/8 -translate-x-1/2 backdrop-blur-md border border-gray-400"
+    class="absolute z-10 left-1/2 top-1/8 w-1/8 hover:w-1/4 transition-all -translate-x-1/2"
     :class="inputFucos ? '!w-1/4' : 'w-1/8'"
   >
-    <input
-      class="w-full"
-      @input="searchInput"
-      v-model="searchText"
-      @focus="inputFucos = true"
-      @blur="inputFucos = false"
-      @keydown.enter="toSearch(searchText)"
-    />
     <div
-      class="absolute top-[calc(100%+10px)] transition-all backdrop-blur-lg bg-white-0.5 left-0 w-full rounded-md"
+      class="w-full bg-white-0.15 rounded-full py-2 px-4 transition-all"
+      style="backdrop-filter: blur(6px)"
+    >
+      <input
+        class="w-full text-gray-300 bg-transparent"
+        @input="searchInput"
+        v-model="searchText"
+        @focus="inputFucos = true"
+        @blur="inputFucos = false"
+        @keydown.enter="toSearch(searchText)"
+      />
+    </div>
+    <div
+      class="mt-4 transition-all w-full rounded-md bg-white-0.15 text-gray-300"
+      style="backdrop-filter: blur(12px)"
       :class="{
         'invisible opacity-0': !inputFucos,
       }"
@@ -34,8 +40,8 @@ import { suSearch } from "@/api/su";
 import searchApi from "@/config";
 import { ref } from "vue";
 
-const searchFrom = ref('bing')
-const suList = ref([]);
+const searchFrom = ref("bing");
+const suList = ref<string[]>([]);
 const searchText = ref("");
 const inputFucos = ref(false);
 let timer: any;
@@ -57,8 +63,8 @@ const searchInput = (e) => {
 };
 
 const toSearch = (keyWord: string) => {
-  window.open(`${searchApi[searchFrom.value]}${keyWord}`)
-  searchText.value = ''
-  suList.value = []
+  window.open(`${searchApi[searchFrom.value]}${keyWord}`);
+  searchText.value = "";
+  suList.value = [];
 };
 </script>
