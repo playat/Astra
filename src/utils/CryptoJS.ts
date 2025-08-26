fe273a1fef2bdeebb6dcd45652384e0e61f0c0a1
chenqian <playat@163.com>
2025-08-26 17:05:54 +0800

    fix: 加密工具类

---
+++ src/utils/CryptoJS.ts
@@ -0,0 +1,27 @@
+import CryptoJS from "crypto-js";
+import JSEncrypt from "jsencrypt";
+
+const publicKey = "";
+// 生成随机AES密钥
+export const generateAesKey = () => {
+  const key = CryptoJS.lib.WordArray.random(16).toString(); // 16字节=128位
+  return key;
+};
+
+// 加密数据
+export const encryptData = (data: { [key: string]: any }) => {
+  const aesKey = generateAesKey();
+  // 用AES加密数据
+  const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(data), aesKey, {
+    mode: CryptoJS.mode.CBC,
+    padding: CryptoJS.pad.Pkcs7,
+    iv: CryptoJS.enc.Utf8.parse("1234567890abcdef"),
+  }).toString();
+
+  // 用RSA加密AES密钥
+  const encrypt = new JSEncrypt.JSEncrypt();
+  encrypt.setPublicKey(publicKey);
+  const encryptedAesKey = encrypt.encrypt(aesKey);
+
+  return encryptedAesKey;
+};
