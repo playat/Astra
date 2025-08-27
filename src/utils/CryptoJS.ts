9a4dc1a3cf4fb1254dfdbad0feb30570893133e8
chenqian <playat@163.com>
2025-08-27 11:34:18 +0800

    fix: 加密

--- src/utils/CryptoJS.ts
+++ src/utils/CryptoJS.ts
@@ -2,27 +2,30 @@ import CryptoJS from "crypto-js";
 import JSEncrypt from "jsencrypt";
 
 const publicKey = "";
-// 生成随机AES密钥
-export const generateAesKey = () => {
-  const key = CryptoJS.lib.WordArray.random(16).toString(); // 16字节=128位
-  return key;
-};
 
 // 加密数据
 export const encryptData = (data: { [key: string]: any }) => {
-  const aesKey = generateAesKey();
+  const aesKey = CryptoJS.lib.WordArray.random(16).toString();
   const iv = CryptoJS.lib.WordArray.random(16);
   // 用AES加密数据
-  const encryptedData = CryptoJS.AES.encrypt(JSON.stringify(data), aesKey, {
-    mode: CryptoJS.mode.CBC,
-    padding: CryptoJS.pad.Pkcs7,
-    iv
-  }).toString();
+  const encryptedData = CryptoJS.AES.encrypt(
+    JSON.stringify(data),
+    CryptoJS.enc.Utf8.parse(aesKey),
+    {
+      mode: CryptoJS.mode.CBC,
+      padding: CryptoJS.pad.Pkcs7,
+      iv,
+    }
+  ).toString();
 
   // 用RSA加密AES密钥
   const encrypt = new JSEncrypt.JSEncrypt();
   encrypt.setPublicKey(publicKey);
   const encryptedAesKey = encrypt.encrypt(aesKey);
 
-  return encryptedAesKey;
+  return {
+    data: encryptedData,
+    key: encryptedAesKey,
+    iv: iv.toString(CryptoJS.enc.Hex),
+  };
 };
