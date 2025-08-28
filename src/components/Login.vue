7c767f4b4a551ae5641dcb98d956d94d6d494b35
chenqian <playat@163.com>
2025-08-28 17:53:23 +0800

    fix: 登录样式调整

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="box">
-    <div class="grid-bg" />
+  <div
+    class="bg-[#0f172a] overflow-hidden w-full h-full flex justify-center items-center fixed left-0 top-0 z-50"
+  >
+    <div class="grid-bg absolute left-0 top-0 w-[200%] h-[200%]" />
     <!-- <div class="py-6 px-8 bg-[rgba(15,23,42,0.7)] rounded-md"> -->
     <div
       class="py-6 px-8 rounded-md"
       style="background-color: rgba(30, 58, 138, 0.1)"
     >
-      <div class="text-white">Enter Password</div>
-      <div class="pb-5 overflow-hidden">
+      <div class="text-[#e2e8f0]">Enter Password</div>
+      <div class="pb-6 overflow-hidden">
         <input
           v-model="pwd"
           type="password"
-          class="password-input"
-          placeholder=""
+          class="password-input relative w-[370px] h-[46px] bg-[#1e293b80] border-b-2 border-[#3b82f680] px-5 text-lg mt-2 text-[#e2e8f0] text-center transition-all"
+          placeholder="8989"
           autocomplete="off"
           autocorrect="off"
           spellcheck="false"
@@ -49,11 +51,6 @@ onMounted(() => {
 
 <style scoped>
 .grid-bg {
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 200%;
-  height: 200%;
   background-image: linear-gradient(rgba(30, 58, 138, 0.1) 1px, transparent 1px),
     linear-gradient(90deg, rgba(30, 58, 138, 0.1) 1px, transparent 1px);
   background-size: 40px 40px;
@@ -69,49 +66,12 @@ onMounted(() => {
   }
 }
 
-.box {
-  background-color: #0f172a;
-  overflow: hidden;
-  height: 100%;
-  width: 100%;
-  display: flex;
-  left: 0;
-  top: 0;
-  justify-content: center;
-  align-items: center;
-  position: fixed;
-  z-index: 50;
-}
-
-.password-input {
-  width: 370px;
-  height: 46px;
-  background-color: #1e293b80;
-  border-bottom: 2px solid rgba(59, 130, 246, 0.5);
-  padding: 0 20px;
-  font-size: 18px;
-  margin-top: 8px;
-  color: #e2e8f0;
-  outline: none;
-  text-align: center;
-  transition: all 0.3s ease;
-  box-shadow: 0 0 10px rgba(59, 130, 246, 0);
-  position: relative;
-  z-index: 1;
-}
-
 .password-input:focus {
-  border-color: rgba(59, 130, 246, 1);
-  box-shadow: 0 5px 3px rgba(59, 130, 246, 0.5);
+  border-color: #3b82f6;
+  box-shadow: 0 5px 20px rgba(59, 130, 246, 0.5);
 }
 
 .password-input::placeholder {
   color: rgba(226, 232, 240, 0.3);
-}
-
-@media (max-width: 640px) {
-  .password-input {
-    width: 85%;
-  }
 }
 </style>
