b36c3afcd550d6c94908aa1f196b2f0edd6822f1
chenqian <playat@163.com>
2025-10-20 14:48:24 +0800

    fix: 优化编译

--- build.sh
+++ build.sh
@@ -5,61 +5,30 @@ set -e
 # 定义目标目录（可修改为你的目标路径）
 TARGET_DIR="/usr/share/nginx/html/TABS/"
 
-echo -e "\033[33m 开始执行前端构建部署... \033[0m"
+echo -e "\033[33m -------------------启动前端构建部署------------------- \033[0m"
+echo -e "目标路径：\033[33m $TARGET_DIR \033[0m"
 
 # 先拉取更改
-echo -e "\033[33m 拉取更改中... \033[0m"
+echo -e "\033[33m 拉取更新... \033[0m"
 git pull --rebase
-
-# 检查是否存在package.json文件
-if [ ! -f "package.json" ]; then
-    echo -e "\033[31m 错误：当前目录下没有找到package.json文件 \033[0m"
-    exit 1
-fi
+echo -e "\033[32m 拉取完成 \033[0m"
 
 # 执行npm install
-echo -e "\033[33m 正在安装依赖... \033[0m"
+echo -e "\033[33m 安装依赖... \033[0m"
 pnpm install
+echo -e "\033[32m 安装完成 \033[0m"
 
 # 执行npm run build
-echo -e "\033[33m 正在构建项目... \033[0m"
+echo -e "\033[33m 构建项目... \033[0m"
 pnpm run build
-
-# 检查dist目录是否存在
-if [ ! -d "dist" ]; then
-    echo -e "\033[31m 错误：构建后未找到dist目录 \033[0m"
-    exit 1
-fi
+echo -e "\033[32m 构建完成 \033[0m"
 
 # 确保目标目录存在
-echo -e "\033[33m 准备目标目录... \033[0m"
+echo -e "\033[33m 准备目录... \033[0m"
 mkdir -p "$TARGET_DIR"
-
-
-# 指定要检查的目录路径
-target_dir="/usr/share/nginx/html/TABS/dist"
-
-# 检查目录是否存在
-if [ -d "$target_dir" ]; then
-    echo -e "\033[33m 目录 $target_dir 存在，准备删除... \033[0m"
-    
-    # 删除目录及其所有内容
-    rm -rf "$target_dir"
-    
-    # 验证删除结果
-    if [ ! -d "$target_dir" ]; then
-        echo -e "\033[32m 目录 $target_dir 已成功删除。 \033[0m"
-    else
-        echo -e "\033[31m 删除目录 $target_dir 失败！ \033[0m"
-        exit 1
-    fi
-else
-    echo -e "\033[36m 目录 $target_dir 不存在，无需删除。 \033[0m"
-fi
+echo -e "\033[32m 准备完成 \033[0m"
 
 # 移动dist目录到目标位置
-echo -e "\033[32m 正在移动dist目录到 $TARGET_DIR \033[0m"
-# 使用mv命令，如果目标目录已存在dist，则覆盖
+echo -e "\033[32m 部署中... \033[0m"
 mv -f dist "$TARGET_DIR"/
-
-echo -e "\033[32m 部署完成！dist目录已移动到 $TARGET_DIR \033[0m"    
+echo -e "\033[32m 部署完成！ \033[0m"    
